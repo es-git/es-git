fa3a5a803f62aba190ab73f93f4ea329c6a93309
Marius Gundersen <me@mariusgundersen.net>
2017-07-23 23:06:46 +0200

    Added zlib library, for writing to disk

---
+++ packages/zlib-mixin/ts/index.test.ts
@@ -0,0 +1,37 @@
+import test from 'ava';
+
+import zlibMixin from './index';
+
+test('save and load tag', async t => {
+  const compresedRepo = new CompressedRepo();
+  const hash = '1234';
+  const expected = 'testing some text';
+  const object = Uint8Array.from(expected.split('').map(x => x.charCodeAt(0)));
+  await compresedRepo.saveRaw(hash, object);
+  const result = await compresedRepo.loadRaw(hash);
+  if(result === undefined){
+    t.fail('result is undefined');
+    return
+  }
+  const actual = String.fromCharCode(...result);
+  t.is(actual, expected);
+});
+
+const CompressedRepo = zlibMixin(class {
+  private readonly map : Map<string, Uint8Array>;
+  constructor(){
+    this.map = new Map<string, Uint8Array>();
+  }
+  async saveRaw(hash : string, object : Uint8Array){
+    this.map.set(hash, object);
+  }
+  async loadRaw(hash : string){
+    return this.map.get(hash);
+  }
+  async readRef(ref : string){
+    return undefined;
+  }
+  async updateRef(ref : string, hash : string){
+    return undefined;
+  }
+});
---
+++ packages/zlib-mixin/ts/index.ts
@@ -0,0 +1,19 @@
+import { Type, Mode, Constructor, IRawRepo, Hash } from '@es-git/core';
+import * as pako from 'pako';
+
+export default function zlibMixin<T extends Constructor<IRawRepo>>(repo : T) : T {
+  return class CompressedRepo extends repo implements IRawRepo {
+    constructor(...args : any[]){
+      super(...args);
+    }
+
+    async saveRaw(hash : Hash, raw : Uint8Array) {
+      await super.saveRaw(hash, pako.deflate(raw));
+    }
+
+    async loadRaw(hash : Hash) {
+      const raw = await super.loadRaw(hash);
+      return raw ? pako.inflate(raw) : undefined;
+    }
+  }
+}
