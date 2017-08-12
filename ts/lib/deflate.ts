60b203085ebca5e3db578f786174966c5fe31d02
Marius Gundersen <me@mariusgundersen.net>
2017-08-13 01:24:02 +0200

    All of this has been implemented now

--- ts/lib/deflate.ts
+++
@@ -1,8 +0,0 @@
-import * as pako from 'pako';
-import {Binary} from 'bodec';
-
-export default Binary === Uint8Array
-  ? pako.deflate
-  : function deflate(value : Uint8Array) {
-    return new Binary(pako.deflate(new Uint8Array(value)));
-  };
--- ts/lib/inflate.ts
+++
@@ -1,8 +0,0 @@
-import * as pako from 'pako';
-import {Binary} from 'bodec';
-
-export default Binary === Uint8Array
-  ? pako.inflate
-  : function inflate(value : Uint8Array) {
-    return new Binary(pako.inflate(new Uint8Array(value)));
-  };
